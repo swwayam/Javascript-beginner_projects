b44dd051f2a3145c6113c2841acc5b35ee4c6d31
ssuryavanshi <ssuryavanshi@pop-os.localdomain>
2021-09-14 19:24:01 +0530

    Created the funcationality of click

--- Random-color-generator/main.js
+++ Random-color-generator/main.js
@@ -5,14 +5,37 @@
 // If user has selected Simple from nav bar the color displayed in the bg its name should appear in the ans.
 // If user selects hex the ans should be a hexadecimal value.
 
-const simpleColors = ["Red", "Blue"];
-const ht = document.querySelector('.content');
-ht.insertAdjacentHTML("afterbegin", `
-<div class="content-display">
-                <h2 class="content__displayText white">
-                    Background Color : <span class="content__displayAns blue">${simpleColors[0]}</span><!--Your color-->
-                </h2>
-            </div>
-`)
+const simpleColors = ["Blue", "Brown", "Orange", "Purple"];
+
+let index = 0;
+let displayColor = 'Red';
+const changeColor = (color) => {
+    displayColor = color[index];
+    if (index === color.length -1) {
+        index = 0;
+    } else {
+        index++
+    }
+    ht.insertAdjacentHTML("beforeend", `
+ <span class="content__displayAns blue">${displayColor}</span>              
+`);
+    displayColor = '';
+}
 
 
+
+const ht = document.querySelector('.content__displayText');
+const button = document.querySelector('.content__displayButton');
+
+
+
+button.addEventListener("click", () => {
+    changeColor(simpleColors);
+});
+
+
+
+
+ht.insertAdjacentHTML("beforeend", `
+<span class="content__displayAns blue">${displayColor}</span>              
+`);
