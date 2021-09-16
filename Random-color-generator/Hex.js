29979d6bae5b238a7e9a97ed1849c18d7f3e989e
ssuryavanshi <ssuryavanshi@pop-os.localdomain>
2021-09-16 21:42:37 +0530

    Hex file added and few modifications done

---
+++ Random-color-generator/Hex.js
@@ -0,0 +1,30 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+
+export const getRandomNumber = (arr) => {
+    let random = Math.floor(Math.random() * arr.length)
+    //console.log(random);
+    return random;
+}
+
+let color = document.querySelector('.content__displayAns');
+console.log(color);
+const clickBtn = document.querySelector('.content__displayButton');
+
+
+
+clickBtn.addEventListener("click", () => {
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++) {
+        let index = hex[getRandomNumber(hex)];
+        hexColor += index;    
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
+
+
+
+
+
+//console.log(Math.random() * 4);
--- Random-color-generator/main.js
+++ Random-color-generator/main.js
@@ -5,22 +5,25 @@
 // If user has selected Simple from nav bar the color displayed in the bg its name should appear in the ans.
 // If user selects hex the ans should be a hexadecimal value.
 
+import { getRandomNumber } from "./Hex";
+
 const simpleColors = ["Blue", "Brown", "Orange", "Purple"];
 
-let index = 0;
-let displayColor = 'Red';
-const changeColor = (color) => {
-    displayColor = color[index];
-    if (index === color.length -1) {
-        index = 0;
-    } else {
-        index++
-    }
-    ht.insertAdjacentHTML("beforeend", `
- <span class="content__displayAns blue">${displayColor}</span>              
-`);
-    displayColor = '';
-}
+// let index = 0;
+
+
+// const changeColor = (color) => {
+//     let displayColor = color[index];
+//     if (index === color.length -1) {
+//         index = 0;
+//     } else {
+//         index++
+//     }
+    
+
+// }
+
+
 
 
 
@@ -30,12 +33,33 @@ const button = document.querySelector('.content__displayButton');
 
 
 button.addEventListener("click", () => {
-    changeColor(simpleColors);
+   let color = simpleColors[ getRandomNumber(simpleColors)];
 });
 
 
+/*
 
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".content__displayAns");
 
-ht.insertAdjacentHTML("beforeend", `
-<span class="content__displayAns blue">${displayColor}</span>              
-`);
+function hi() {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  // console.log(hexColor);
+
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+};
+
+function getRandomNumber() {
+  
+}
+
+console.log(Math.floor(Math.random() * hex.length));
+
+hi();
+
+*/
