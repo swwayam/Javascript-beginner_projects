96d8b3695cd97d7b7715ca0190fa277ddce0a28a
ssuryavanshi <ssuryavanshi@pop-os.localdomain>
2021-09-20 20:57:57 +0530

    Completed the project, removed all the bugs and added some styles

--- Random-color-generator/main.js
+++ Random-color-generator/main.js
@@ -5,61 +5,172 @@
 // If user has selected Simple from nav bar the color displayed in the bg its name should appear in the ans.
 // If user selects hex the ans should be a hexadecimal value.
 
-import { getRandomNumber } from "./Hex";
-
-const simpleColors = ["Blue", "Brown", "Orange", "Purple"];
-
-// let index = 0;
 
 
-// const changeColor = (color) => {
-//     let displayColor = color[index];
-//     if (index === color.length -1) {
-//         index = 0;
-//     } else {
-//         index++
-//     }
-    
+const simpleColors = [  "AliceBlue",
+"AntiqueWhite",
+"Aqua",
+"Aquamarine",
+"Azure",
+"Beige",
+"Bisque",
+"Black",
+"BlanchedAlmond",
+"Blue",
+"BlueViolet",
+"Brown",
+"BurlyWood",
+"CadetBlue",
+"Chartreuse",
+"Chocolate",
+"Coral",
+"CornflowerBlue",
+"Cornsilk",
+"Crimson",
+"Cyan",
+"DarkBlue",
+"DarkCyan",
+"DarkGoldenRod",
+"DarkGray",
+"DarkGrey",
+"DarkGreen",
+"DarkKhaki",
+"DarkMagenta",
+"DarkOliveGreen",
+"DarkOrange",
+"DarkOrchid",
+"DarkRed",
+"DarkSalmon",
+"DarkSeaGreen",
+"DarkSlateBlue",
+"DarkSlateGray",
+"DarkSlateGrey",
+"DarkTurquoise",
+"DarkViolet",
+"DeepPink",
+"DeepSkyBlue",
+"DimGray",
+"DimGrey",
+"DodgerBlue",
+"FireBrick",
+"FloralWhite",
+"ForestGreen",
+"Fuchsia",
+"Gainsboro",
+"GhostWhite",
+"Gold",
+"GoldenRod",
+"Gray",
+"Grey",
+"Green",
+"GreenYellow",
+"HoneyDew",
+"HotPink",
+"IndianRed",
+"Indigo",
+"Ivory",
+"Khaki",
+"Lavender",
+"LavenderBlush",
+"LawnGreen",
+"LemonChiffon",
+"LightBlue",
+"LightCoral",
+"LightCyan",
+"LightGoldenRodYellow",
+"LightGray",
+"LightGrey",
+"LightGreen",
+"LightPink",
+"LightSalmon",
+"LightSeaGreen",
+"LightSkyBlue",
+"LightSlateGray",
+"LightSlateGrey",
+"LightSteelBlue",
+"LightYellow",
+"Lime",
+"LimeGreen",
+"Linen",
+"Magenta",
+"Maroon",
+"MediumAquaMarine",
+"MediumBlue",
+"MediumOrchid",
+"MediumPurple",
+"MediumSeaGreen",
+"MediumSlateBlue",
+"MediumSpringGreen",
+"MediumTurquoise",
+"MediumVioletRed",
+"MidnightBlue",
+"MintCream",
+"MistyRose",
+"Moccasin",
+"NavajoWhite",
+"Navy",
+"OldLace",
+"Olive",
+"OliveDrab",
+"Orange",
+"OrangeRed",
+"Orchid",
+"PaleGoldenRod",
+"PaleGreen",
+"PaleTurquoise",
+"PaleVioletRed",
+"PapayaWhip",
+"PeachPuff",
+"Peru",
+"Pink",
+"Plum",
+"PowderBlue",
+"Purple",
+"RebeccaPurple",
+"Red",
+"RosyBrown",
+"RoyalBlue",
+"SaddleBrown",
+"Salmon",
+"SandyBrown",
+"SeaGreen",
+"SeaShell",
+"Sienna",
+"Silver",
+"SkyBlue",
+"SlateBlue",
+"SlateGray",
+"SlateGrey",
+"Snow",
+"SpringGreen",
+"SteelBlue",
+"Tan",
+"Teal",
+"Thistle",
+"Tomato",
+"Turquoise",
+"Violet",
+"Wheat",
+"White",
+"WhiteSmoke",
+"Yellow",
+"YellowGreen",
+];
 
-// }
 
-
-
-
-
-const ht = document.querySelector('.content__displayText');
+const ht = document.querySelector('.content__displayAns');
 const button = document.querySelector('.content__displayButton');
 
 
 
 button.addEventListener("click", () => {
-   let color = simpleColors[ getRandomNumber(simpleColors)];
+  let color = simpleColors[getRandomNumber()];
+  ht.textContent = color;
+  document.body.style.backgroundColor = color;
 });
 
-
-/*
-
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".content__displayAns");
-
-function hi() {
-  let hexColor = "#";
-  for (let i = 0; i < 6; i++) {
-    hexColor += hex[getRandomNumber()];
-  }
-  // console.log(hexColor);
-
-  color.textContent = hexColor;
-  document.body.style.backgroundColor = hexColor;
-};
-
-function getRandomNumber() {
-  
+const getRandomNumber = () => {
+  let random = Math.floor(Math.random() * simpleColors.length)
+  //console.log(random);
+  return random;
 }
-
-console.log(Math.floor(Math.random() * hex.length));
-
-hi();
-
-*/
